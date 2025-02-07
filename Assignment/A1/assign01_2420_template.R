10d6e35113663224b564caf6a3daf5ed3d2f3b71
jhqiu21 <142405039+jhqiu21@users.noreply.github.com>
2025-02-07 15:33:42 +0800

    Add Assignment 1

---
+++ Assignment/A1/assign01_2420_template.R
@@ -0,0 +1,9 @@
+## Do not edit lines 4 and 5 below.
+## You may decide not to use readLines(), but the path to the data file must
+## remain as it is:
+path_to_dat <- "data/aircraft_failure.txt"
+tmp <- readLines(path_to_dat, warn = FALSE)
+## Do not edit the lines above.
+###############################################################################
+
+## YOUR CODE HERE:
