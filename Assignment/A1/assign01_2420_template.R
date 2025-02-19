002926c55c875f818fd11f4c17bdfd2355ae2ce6
jhqiu21 <142405039+jhqiu21@users.noreply.github.com>
2025-02-19 22:01:54 +0800

    attemp A1

--- Assignment/A1/assign01_2420_template.R
+++ Assignment/A1/assign01_2420_template.R
@@ -7,3 +7,50 @@ tmp <- readLines(path_to_dat, warn = FALSE)
 ###############################################################################
 
 ## YOUR CODE HERE:
+
+## QUESTION 1
+list = strsplit(tmp, " ")
+ftimes_df <- do.call(rbind, lapply(seq_along(list), function(i) {
+  data.frame(aircraft = i, failure_times = as.numeric(unlist(list[[i]])))
+}))
+
+## QUESTION 2
+library(lattice)
+histogram(~failure_times | aircraft, data=ftimes_df,
+          layout = c(5, 2),
+          as.table=TRUE,
+          main = "Failure Times Distribution by Aircraft",
+          xlab = "Failure Times",
+          ylab = "Frequency")
+
+## QUESTION 3
+mom_gamma <- function(data) {
+  xbar = mean(data)
+  sx2 = var(data)
+  alpha_hat = (xbar^2) / sx2
+  beta_hat = sx2 / xbar
+  return (list(alpha = alpha_hat, beta = beta_hat))
+}
+
+ac1 <- ftimes_df$failure_times[ftimes_df$aircraft == 1]
+mom_gamma(ac1)
+
+## QUESTION 4
+ac_vec <- numeric(10)
+for (i in 1:10) {
+  x <- ftimes_df$failure_times[ftimes_df$aircraft == i]
+  if (length(x) > 1) {
+    ac_vec[i] <- cor(x[-length(x)], x[-1])
+  } else {
+    ac_vec[i] <- NULL
+  }
+}
+ac_vec
+
+
+
+
+
+
+
+
